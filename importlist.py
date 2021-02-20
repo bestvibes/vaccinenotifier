23d6bf661077c89aa534c914aba62d45f0ef72d9
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2021-02-20 00:25:15 -0800

    importlist: fetch responses from gforms

---
+++ importlist.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+SPREADSHEET_ID = os.environ["GSHEETS_SPREADSHEET_ID"]
+RANGE_NAME = 'Form Responses 1'
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        for i,row in enumerate(values):
+            if i > 0:
+                row[5] = "+1"+row[5]
+            print(','.join(row))
+        print(f'Found {len(values)-1} rows. Exporting...')
+        with open('sheets.csv', 'w+') as f:
+            f.write('\n'.join(map(lambda x: ','.join(x[1:-1]), values[1:]))+'\n')
+
+if __name__ == '__main__':
+    main()
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-source twilio.sh # add keys to env
+source env.sh
 
 set -xuo pipefail
 
 . ./venv/bin/activate
 while true; do
-    python3 processlist.py list.csv
+    python3 processlist.py custom.csv sheets.csv
     while IFS=, read -r age ind county zip num; do
         python3 scraper.py "$age" "$ind" "$county" "$zip" "$num"
     done < clean_list.csv
