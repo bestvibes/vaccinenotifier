c31331178cbd67d96f6a610c93178a936e37f60e
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2021-02-21 22:44:38 -0800

    run.sh: only echo relevant commands, print runtime

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source env.sh
 
-set -xuo pipefail
+set -uo pipefail
 
 if [ $# -eq 0 ]; then
     echo "specify input data!"
@@ -10,15 +10,15 @@ fi
 
 . ./venv/bin/activate
 while true; do
-    python3 processlist.py $@
+    (set -x; python3 processlist.py $@)
     texted=0
     while IFS=, read -r age ind county zip num; do
-        python3 scraper.py "$age" "$ind" "$county" "$zip" "$num"
+        (set -x; time python3 scraper.py "$age" "$ind" "$county" "$zip" "$num")
         if [ $? != 0 ] && [ $texted == 0 ]; then
             ./textme.sh ERROR: run broken for "$age" "$ind" "$county" "$zip"
             texted=1
         fi
     done < clean_list.csv
     date
-    sleep 900
+    (set -x; sleep 900)
 done
