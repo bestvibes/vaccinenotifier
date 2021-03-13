c577522a9985cf5d2fbae7d35d1eb2aa2a41df7a
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2021-03-12 19:19:26 -0800

    add disability to logs

--- scraper.py
+++ scraper.py
@@ -87,7 +87,7 @@ def main():
     age = age_to_range(age)
 
     try:
-        print(f"Checking for age={age} ind={industry} county={county} zip={zipcode} und={undcondition} at {currtime}\nrecipients={recipients}")
+        print(f"Checking for age={age} ind={industry} county={county} zip={zipcode} und={undcondition} dis={disability} at {currtime}\nrecipients={recipients}")
         URL = 'https://myturn.ca.gov'
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
@@ -118,9 +118,9 @@ def main():
         signuplink = os.environ['SIGNUP_LINK']
         tclient = Client(account_sid, auth_token)
         if 'ineligible' in driver.current_url:
-            print(f"INELIGIBLE for age={age} county={county} industry={industry} und={undcondition}")
+            print(f"INELIGIBLE for age={age} county={county} industry={industry} und={undcondition} dis={disability}")
         elif 'location' in driver.current_url:
-            print(f"ELIGIBLE for age={age} county={county} industry={industry} und={undcondition}")
+            print(f"ELIGIBLE for age={age} county={county} industry={industry} und={undcondition} dis={disability}")
 
             get_element(wait, "//input[@id='location-search-input']").send_keys(zipcode+Keys.RETURN)
             WebDriverWait(driver, 2).until(EC.invisibility_of_element((By.XPATH, "//div[class='loader-background']")))
@@ -176,7 +176,7 @@ def main():
         else:
             print("IDK WHERE I AM:", driver.current_url)
             message = tclient.messages.create(
-                body=f"Notifier failed for {age} in {industry} at {county},{zipcode} und={undcondition} at {currtime}!",
+                body=f"Notifier failed for {age} in {industry} at {county},{zipcode} und={undcondition} dis={disability} at {currtime}!",
                 from_=fromnumber,
                 to=maintainernum
             )
